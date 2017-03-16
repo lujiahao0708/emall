d7467580691213db883c09b4a596e06ca4afc1f2
chiahaolu <xinruodingshui@gmail.com>
2017-03-17 00:21:45 +0800

    添加图片上传功能  对应的ftp的docker还没有弄好

---
+++ taotao-manager/src/main/java/com/lujiahao/manager/controller/PictureController.java
@@ -0,0 +1,31 @@
+package com.lujiahao.manager.controller;
+
+import com.lujiahao.common.utils.JsonUtils;
+import com.lujiahao.manager.service.PictureService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+/**
+ * 图片上传controller
+ * Created by ljh on 2017/3/17.
+ */
+@Controller
+public class PictureController {
+    @Autowired
+    private PictureService pictureService;
+
+    @RequestMapping(value = "/pic/upload")
+    @ResponseBody
+    public String pictureUpload(MultipartFile uploadFile){
+        Map result = pictureService.uploadPicture(uploadFile);
+        // 如果直接返回map的话,在firefox中是无法使用的 侧面看出此插件的兼容性是有问题的
+        String json = JsonUtils.objectToJson(result);
+        return json;
+    }
+}
---
+++ taotao-manager/src/main/java/com/lujiahao/manager/service/impl/PictureServiceImpl.java
@@ -0,0 +1,63 @@
+package com.lujiahao.manager.service.impl;
+
+import com.lujiahao.common.utils.FtpUtil;
+import com.lujiahao.common.utils.IDUtils;
+import com.lujiahao.manager.service.PictureService;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ljh on 2017/3/16.
+ */
+@Service
+public class PictureServiceImpl implements PictureService {
+
+    @Value("${FTP_ADDRESS}")
+    private String FTP_ADDRESS;
+    @Value("${FTP_PORT}")
+    private Integer FTP_PORT;
+    @Value("${FTP_USERNAME}")
+    private String FTP_USERNAME;
+    @Value("${FTP_PASSWORD}")
+    private String FTP_PASSWORD;
+    @Value("${FTP_BASE_PATH}")
+    private String FTP_BASE_PATH;
+    @Value("${IMAGE_BASE_URL}")
+    private String IMAGE_BASE_URL;
+
+    @Override
+    public Map uploadPicture(MultipartFile uploadFile) {
+        Map resultMap = new HashMap();
+        try {
+            // 生成一个新的文件名
+            // 取原始文件名
+            String oldName = uploadFile.getOriginalFilename();
+            // 生成新文件名
+            String newName = IDUtils.genImageName();
+            newName = newName + oldName.substring(oldName.lastIndexOf("."));
+            // 图片上传
+            String imagePath = new DateTime().toString("/yyyy/MM/dd");
+            boolean result = FtpUtil.uploadFile(FTP_ADDRESS, FTP_PORT, FTP_USERNAME, FTP_PASSWORD,
+                    FTP_BASE_PATH, imagePath, newName, uploadFile.getInputStream());
+            // 返回结果
+            if (!result) {
+                resultMap.put("error",1);
+                resultMap.put("message","文件上传失败");
+                return resultMap;
+            }
+            resultMap.put("error",0);
+            resultMap.put("url",IMAGE_BASE_URL + imagePath + "/" + newName);
+            return resultMap;
+        } catch (Exception e) {
+            resultMap.put("error",1);
+            resultMap.put("message","文件上传失败");
+            return resultMap;
+        }
+    }
+}
