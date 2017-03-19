e5729579ff9dfaf2720aa4fe577b45559014a95a
chiahaolu <xinruodingshui@gmail.com>
2017-03-19 23:43:09 +0800

    完成图片服务器的docker化

--- taotao-common/src/main/java/com/lujiahao/common/utils/FtpUtil.java
+++ taotao-common/src/main/java/com/lujiahao/common/utils/FtpUtil.java
@@ -143,8 +143,8 @@ public class FtpUtil {
 	
 	public static void main(String[] args) {
 		try {  
-	        FileInputStream in=new FileInputStream(new File("D:\\temp\\image\\gaigeming.jpg"));  
-	        boolean flag = uploadFile("192.168.25.133", 21, "ftpuser", "ftpuser", "/home/ftpuser/www/images","/2015/01/21", "gaigeming.jpg", in);  
+	        FileInputStream in=new FileInputStream(new File("D:\\temp\\image\\gaigeming.jpg"));
+	        boolean flag = uploadFile("192.168.25.133", 21, "ftpuser", "ftpuser", "/home/ftpuser/www/images","/2015/01/21", "gaigeming.jpg", in);
 	        System.out.println(flag);  
 	    } catch (FileNotFoundException e) {  
 	        e.printStackTrace();  
