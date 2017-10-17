d8ed49e8045dcaa438f7260b7f3380096b35c688
chiahaolu <lujiahao0708@gmail.com>
2017-10-17 18:11:32 +0800

    [emall-sso] sso模块恢复,此模块只做单点相关,权限交由emall-auth模块

---
+++ emall-sso/src/main/java/com/lujiahao/sso/domain/UserDTO.java
@@ -0,0 +1,24 @@
+package com.lujiahao.sso.domain;
+
+import lombok.Data;
+
+/**
+ * 用户登录参数封装bean
+ * @author lujiahao
+ * @version 1.0
+ * @date 2017-10-10 17:00
+ */
+@Data
+public class UserDTO {
+    private Long id;
+
+    // 用户名
+    private String username;
+    // 密码
+    private String password;
+    // 手机号
+    private String phone;
+    // 邮箱
+    private String email;
+
+}
---
+++ emall-sso/src/main/java/com/lujiahao/sso/service/UserService.java
@@ -0,0 +1,38 @@
+package com.lujiahao.sso.service;
+
+
+import com.lujiahao.sso.domain.UserDTO;
+import com.lujiahao.common.pojo.CommonResult;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by lujiahao on 2016/10/31.
+ */
+public interface UserService {
+    /**
+     * 校验数据
+     * @param content 数据内容
+     * @param type 数据类型
+     * @return
+     */
+    CommonResult checkData(String content, Integer type);
+
+    /**
+     * 创建用户
+     */
+    int createUser(UserDTO userDTO);
+
+    /**
+     * 用户登录
+     */
+    CommonResult userLogin(UserDTO userDTO, HttpServletRequest request, HttpServletResponse response);
+
+    /**
+     * 根据token查询用户信息
+     * @param token
+     * @return
+     */
+    CommonResult getUserByToken(String token);
+}
